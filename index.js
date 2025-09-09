e43ad6ea3b50de3880ec0af669783b6aa047c226
Sayem-75 <ehtesam.alhaqq@gmail.com>
2025-09-08 21:52:35 -0600

    Home page and features implemented

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Project initialized successfully.");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const posts = []
+
+app.get("/", (req, res) => {
+    // res.send("Project initialized successfully.");
+    res.render("home.ejs", { posts: posts });
+})
+
+app.post("/post", (req, res) => {
+  const post = {
+    title: req.body["title"],
+    body: req.body["body"]
+  }
+
+  posts.push(post);
+
+  res.redirect("/");
 })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+
