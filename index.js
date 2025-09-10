4938314ab4bb01851238b8c43463904d836b548e
Sayem-75 <ehtesam.alhaqq@gmail.com>
2025-09-09 21:34:28 -0600

    Post view page implemented

--- index.js
+++ index.js
@@ -4,31 +4,40 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const posts = []
 
 app.get("/", (req, res) => {
-    // res.send("Project initialized successfully.");
-    res.render("home.ejs", { posts: posts });
-})
+    res.render("home", { posts: posts });
+});
+
+app.get('/post/:index', (req, res) => {
+  const index = req.params.index;
+  const post = posts[index];
+
+  if (post) {
+    res.render("post", { post: post });
+  } else {
+    res.send("Post not found.");
+  }
+});
 
 app.post("/post", (req, res) => {
   const post = {
     title: req.body["title"],
-    body: req.body["body"]
+    body: req.body["body"],
+    date: new Date()
   }
 
   posts.push(post);
 
   res.redirect("/");
-})
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-
 
